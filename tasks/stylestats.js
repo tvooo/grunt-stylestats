b8f194b654a053d4a7bc6f70c06d0ed4896912bf
Tim von Oldenburg <tim@tvooo.de>
2014-03-18 13:23:18 +0100

    Updated dependencies

--- tasks/stylestats.js
+++ tasks/stylestats.js
@@ -23,7 +23,6 @@ module.exports = function(grunt) {
     var options = this.options( require('stylestats/lib/defaultOptions'));
     this.files.forEach(function(f) {
       f.src.forEach( function( file ) {
-        var result = new stylestats(file, options).parse();
         var table = new Table({
           style: {
             head: ['cyan'],
@@ -35,29 +34,32 @@ module.exports = function(grunt) {
             'right': '' , 'right-mid': '' , 'middle': ' '
           }
         });
-        Object.keys(result).forEach(function(key) {
-          var stats = {};
-          var prop = _(_(key).humanize()).titleize();
-          if (key === 'propertiesCount') {
-            var array = [];
-            result[key].forEach(function(item) {
-                array.push([item.property, item.count]);
-            });
-            stats[prop] = array.join('\n').replace(/\,/g, ': ');
-          } else if (key === 'size' || key === 'gzippedSize') {
-            stats[prop] = numeral(result[key]).format('0.0b');
-          } else if (key === 'simplicity') {
-            stats[prop] = numeral(result[key]).format('0.00%');
-          } else {
-            stats[prop] = _.isArray(result[key]) ? result[key].join('\n') : result[key];
-            if (stats[prop] === '') {
-                stats[prop] = 'N/A';
+        var stats = new stylestats(file, options);
+        stats.parse( function( result ) {
+          Object.keys(result).forEach(function(key) {
+            var stats = {};
+            var prop = _(_(key).humanize()).titleize();
+            if (key === 'propertiesCount') {
+              var array = [];
+              result[key].forEach(function(item) {
+                  array.push([item.property, item.count]);
+              });
+              stats[prop] = array.join('\n').replace(/\,/g, ': ');
+            } else if (key === 'size' || key === 'gzippedSize') {
+              stats[prop] = numeral(result[key]).format('0.0b');
+            } else if (key === 'simplicity') {
+              stats[prop] = numeral(result[key]).format('0.00%');
+            } else {
+              stats[prop] = _.isArray(result[key]) ? result[key].join('\n') : result[key];
+              if (stats[prop] === '') {
+                  stats[prop] = 'N/A';
+              }
             }
-          }
-          table.push(stats);
+            table.push(stats);
+          });
+          grunt.log.subhead( file );
+          grunt.log.write( table.toString() );
         });
-        grunt.log.subhead( file );
-        grunt.log.write( table.toString() );
       });
     });
   });
